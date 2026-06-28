bd7e1697229e240c4c9b19f8b18d0beeee4c1cc6
MACHAN <machan@unknownba884ca80ab8.home>
2026-06-28 15:36:21 +0300

    feat: Add loading boundaries to portal and admin layouts for instant UI feedback

---
+++ src/app/(admin)/loading.tsx
@@ -0,0 +1,5 @@
+import { SayfaYukleniyor } from '@/components/arayuz/Yukleniyor'
+
+export default function AdminLoading() {
+  return <SayfaYukleniyor />
+}
---
+++ src/app/(portal)/loading.tsx
@@ -0,0 +1,5 @@
+import { SayfaYukleniyor } from '@/components/arayuz/Yukleniyor'
+
+export default function PortalLoading() {
+  return <SayfaYukleniyor />
+}
